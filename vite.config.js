13eeac08d2c98acf5a9685a81fa2da175e8fe56b
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-10-01 13:06:18 -0300

    chore: configure alias to simplify imports

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// External Library
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, "src/components"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@types": path.resolve(__dirname, "src/types"),
+            "@utils": path.resolve(__dirname, "src/utils"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+        },
+    },
+});
