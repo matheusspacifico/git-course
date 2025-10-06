409b28f53c8715860a1c550cd15f28a0e4af8f93
Erick Santinon Gomes <erick.dicksan@gmail.com>
2025-10-06 20:21:23 -0300

    calros

--- src/assets/content/arena.ts
+++ src/assets/content/arena.ts
@@ -6,9 +6,9 @@ export const areaContent: ArenaState[] = [
     round: "2025-10-01-1",
     attribute: "carisma",
     deckA: "a-prof-ricardo",
-    deckB: "b-profa-renata",
+    deckB: "b-prof-paulo",
     playerAName: "Ana",
-    playerBName: "Bruno",
+    playerBName: "Erikco",
   },
 ];
 
